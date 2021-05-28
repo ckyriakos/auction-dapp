b1cba7a11879980ff2264bda1e8b0631c9f34799
ckyriakos <ckyriakos99@gmail.com>
2021-05-28 07:29:29 -0700

    Updates

---
+++ precache-manifest.c3ce4ada22e1d3fce4c6cd9b739ab41f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7992599414e1cbe26fbdc3b1f96b4bff",
+    "url": "/auction-dapp/index.html"
+  },
+  {
+    "revision": "498616e3c1f700d22092",
+    "url": "/auction-dapp/static/css/main.f083180f.chunk.css"
+  },
+  {
+    "revision": "dbd62ae2a43765cf7215",
+    "url": "/auction-dapp/static/js/2.2c445e0e.chunk.js"
+  },
+  {
+    "revision": "498616e3c1f700d22092",
+    "url": "/auction-dapp/static/js/main.ef9f4831.chunk.js"
+  },
+  {
+    "revision": "4335d4238bf3347cf635",
+    "url": "/auction-dapp/static/js/runtime-main.cb949a02.js"
+  }
+]);
